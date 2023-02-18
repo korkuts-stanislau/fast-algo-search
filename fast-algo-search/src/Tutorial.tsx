e79683184a5c7eca6fc78967496d9b91f9ff4118
Stanislau Korkuts <stkorkuts@gmail.com>
2023-02-18 15:21:42 +0300

    add responsiveness for latest changes

--- fast-algo-search/src/Tutorial.tsx
+++ fast-algo-search/src/Tutorial.tsx
@@ -9,19 +9,19 @@ const buttonClassName = "text-green-300"
 export const Tutorial: React.FC<TutorialProps> = (props) => {
     const [opened, setOpened] = useState(false);
 
-    return <div className="mt-6">
-        <h1 className="text-2xl text-center cursor-default">
-            Tutorial
-            {!opened && <span className="mx-4 bg-green-300 text-black px-[4px] py-[2px] rounded-sm cursor-pointer"
+    return <div className="mt-6 lg:mb-0 mb-2">
+        <h1 className="text-2xl lg:text-center cursor-default mx-10">
+            Hotkeys tutorial
+            {!opened && <span className="inline-block mx-4 bg-green-300 text-black px-[4px] py-[2px] rounded-sm cursor-pointer text-xl -translate-y-[1px]"
                                 onClick={() => setOpened(true)}>
                 Open
             </span>}
-            {opened && <span className="mx-4 bg-red-300 text-black px-[4px] py-[2px] rounded-sm cursor-pointer"
+            {opened && <span className="inline-block mx-4 bg-red-300 text-black px-[4px] py-[2px] rounded-sm cursor-pointer text-xl -translate-y-[1px]"
                                 onClick={() => setOpened(false)}>
                 Close
             </span>}
         </h1>
-        {opened && <div className="bg-gray-600 rounded-md m-auto w-[500px] mt-4 p-4">
+        {opened && <div className="bg-gray-600 rounded-md m-auto lg:w-[500px] w-[85%] mt-4 p-4">
             <ul className="list-disc px-4 cursor-default">
                 <li>To pause/resume video press <span className={buttonClassName}>Space</span></li>
                 <li>To mute video press <span className={buttonClassName}>M</span></li>
